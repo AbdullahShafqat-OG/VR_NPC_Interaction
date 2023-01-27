d14425b971d6ef518e65f87d2b1c13d242199241
Abdullah Shafqat <abdullahshafqat27@hotmail.com>
2023-01-28 03:01:47 +0500

    Fixed stuck hands issue

--- Assets/Scripts/HMDInfoManager.cs
+++ Assets/Scripts/HMDInfoManager.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 public class HMDInfoManager : MonoBehaviour
 {
+    [SerializeField]
+    private XRDeviceSimulator _simulator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,7 @@ public class HMDInfoManager : MonoBehaviour
         else if (XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == "MockHMD Display"))
         {
             Debug.Log("Using Mock HMD");
+            _simulator.gameObject.SetActive(true);
         }
         else
         {
